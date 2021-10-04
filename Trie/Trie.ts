abc4ab17c61f31c5055e86792d4491f173f3016f
Richard Lee <leerw4@gmail.com>
2021-10-03 20:50:04 -0400

    find words from prefix

--- Trie/Trie.ts
+++ Trie/Trie.ts
@@ -21,6 +21,7 @@ export class Trie {
       }
       crawlNode = crawlNode.children[index];
     }
+    crawlNode.word = key;
     crawlNode.isEndOfWord = true;
   }
 
@@ -41,9 +42,66 @@ export class Trie {
 
     return crawlNode.isEndOfWord;
   }
+
+  containsPrefix(prefix: string): boolean {
+    let index: number;
+    let crawlNode = this.root;
+
+    for (let i = 0; i < prefix.length; i++) {
+      index = prefix.toLowerCase().charCodeAt(i) - 'a'.charCodeAt(0);
+      if (crawlNode.children[index] === null) {
+        return false;
+      }
+      crawlNode = crawlNode.children[index];
+    }
+
+    return true;
+  }
+
+  /**
+   * Returns the list of words that contain the prefix
+   */
+  wordsFromPrefix(prefix: string): string[] {
+    // Step 1: Find end node for prefix
+    let crawlNode = this.root;
+
+    for (let i = 0; i < prefix.length; i++) {
+      let index = prefix.charCodeAt(i) - 'a'.charCodeAt(0);
+      // If the prefix is not in the trie, return an empty array
+      if (crawlNode.children[index] === null) {
+        return [];
+      }
+      crawlNode = crawlNode.children[index];
+    }
+
+    // Step 2: Find all children (recursively)
+    return this.findWordsFromPrefixNode(crawlNode);
+  }
+
+  findWordsFromPrefixNode(node: TrieNode): string[] {
+    let crawlNode = node;
+    let words: string[] = [];
+    for (let i = 0; i < crawlNode.children.length; i++) {
+      // If the child is null, we don't need to find it's children
+      if (!crawlNode.children[i]) {
+        continue;
+      }
+
+      // If child is the end of a word, add that word
+      if (crawlNode.children[i].word) {
+        words.push(crawlNode.children[i].word as string);
+      }
+
+      // Recurse on children of the node
+      words.push(...this.findWordsFromPrefixNode(crawlNode.children[i]))
+    }
+
+    return words;
+  }
 }
 
 export class TrieNode {
   children: TrieNode[] = new Array(26).fill(null);
+  word: string | null = null;
   isEndOfWord: boolean = false;
 }
--- Trie/trie-example.ts
+++ Trie/trie-example.ts
@@ -4,7 +4,11 @@ const trie1 = new Trie();
 trie1.insert('eat');
 trie1.insert('ear');
 trie1.insert('earn');
+trie1.insert('earnest');
+trie1.insert('earned');
+trie1.insert('earner');
 trie1.insert('bet');
 
 console.log(trie1.search('eat'));
 console.log(trie1.search('beat'));
+console.log(trie1.wordsFromPrefix('ea'));
